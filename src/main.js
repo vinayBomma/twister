8b21865e71c01f495a32de4a5bdd21629300a7cc
vinayBomma <vinaysb2000@gmail.com>
2019-05-21 17:52:13 +0530

    txt files data parsing

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
+// import hello from '@/assets/helloworld.txt';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount('#app')
+
+
+
+// console.log(hello)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,27 +4,15 @@
       <v-layout row wrap>
         <v-flex xs12 sm6 md4 pa-3>
           <v-card>
-            <v-card-title>Lorem Ipsum</v-card-title>
-            <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro exercitationem repellendus delectus asperiores! Debitis quis, reprehenderit consequatur dolorem nisi deleniti, quae, soluta at cupiditate officia quo doloremque corrupti quod ullam?</v-card-text>
+            <!-- <v-card-title>Lorem Ipsum</v-card-title> -->
+            <!-- <v-card-text>{{ text.slice(0, text.indexOf('=')) }}</v-card-text> -->
           </v-card>
         </v-flex>
         <v-flex xs12 sm6 md4 pa-3>
-          <v-card>
+          <!-- <v-card>
             <v-card-title>Lorem Ipsum</v-card-title>
             <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro exercitationem repellendus delectus asperiores! Debitis quis, reprehenderit consequatur dolorem nisi deleniti, quae, soluta at cupiditate officia quo doloremque corrupti quod ullam?</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex xs12 sm6 md4 pa-3>
-          <v-card>
-            <v-card-title>Lorem Ipsum</v-card-title>
-            <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro exercitationem repellendus delectus asperiores! Debitis quis, reprehenderit consequatur dolorem nisi deleniti, quae, soluta at cupiditate officia quo doloremque corrupti quod ullam?</v-card-text>
-          </v-card>
-        </v-flex>
-        <v-flex xs12 sm6 md4 pa-3>
-          <v-card>
-            <v-card-title>Lorem Ipsum</v-card-title>
-            <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro exercitationem repellendus delectus asperiores! Debitis quis, reprehenderit consequatur dolorem nisi deleniti, quae, soluta at cupiditate officia quo doloremque corrupti quod ullam?</v-card-text>
-          </v-card>
+          </v-card> -->
         </v-flex>
       </v-layout>
     </v-container>
@@ -32,11 +20,38 @@
 </template>
 
 <script>
+// import hello from '!raw-loader!./assets/helloworld.txt';
+import hello from '!raw-loader!../assets/English.txt'
 export default {
+  components: {
+    // hello
+  },
   data () {
     return {
 
     }
+  },
+  methods: {
+
   }
 };
+
+let awee;
+let test = hello.replace(/=========================/g, '')
+let some = test.split('\n')
+awee = some.forEach(someFilter)
+
+function someFilter(item, index){
+  if (item == "\r")
+    console.log(`awesome ${index}`)
+  else
+    console.log(`[${index}]: ${item}`)
+}
+
+// let val = some.includes('\r')
+// console.log(awee)
+// console.log(test)  
+// console.log(some.filter())
+console.log(some)
+// console.log('From Home: ', hello)
 </script>
